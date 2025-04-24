fd0b9e75d25edcdcb93165c2819bad4b238133f3
Brady Herbert <kbradyherbert@gmail.com>
2025-04-24 15:04:49 -0400

    feat(neovim): add bufferline and tab keymaps

---
+++ neovim/.config/nvim/lua/bradiate/plugins/tabs.lua
@@ -0,0 +1,8 @@
+return {
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons', opts = {
+    options = {
+      mode = "tabs",
+      separator_style = "thin",
+    },
+  }},
+}
