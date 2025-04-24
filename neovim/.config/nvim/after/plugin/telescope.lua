db40e3cf5f47d1fd51af551ff1e21650c6cf70d6
Brady Herbert <kbradyherbert@gmail.com>
2025-04-24 15:01:39 -0400

    feat(neovim): add additional keymaps for telescope

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,2 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.git_files, { desc = 'Telescope find git files' })
+
+-- Git
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git Status" })
+
+-- Files
+vim.keymap.set('n', '<leader>pf', function()
+  builtin.find_files({ hidden = true })
+end, { desc = 'Telescope find all files (including hidden)' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope list buffers' })
+vim.keymap.set('n', '<leader>pr', builtin.oldfiles, { desc = 'Telescope find recent files' })
--- neovim/.config/nvim/lua/bradiate/plugins/fuzzy-finder.lua
+++ neovim/.config/nvim/lua/bradiate/plugins/fuzzy-finder.lua
@@ -1,5 +1,25 @@
 return {
 	'nvim-telescope/telescope.nvim',
 	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local telescope = require('telescope')
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        file_ignore_patterns = {
+          "node_modules",
+          ".git/",
+          ".hg/",
+          ".svn/",
+          "__pycache__",
+          "%.egg-info",
+          "%.egg",
+          "%.tar.gz",
+          "%.zip",
+        },
+      }
+    })
+  end,
 }
