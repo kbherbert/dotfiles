f914de251de4cf5b5c35ccf3adc3595b50f9595f
Brady Herbert <kbradyherbert@gmail.com>
2025-04-24 15:04:49 -0400

    feat(neovim): add bufferline and tab keymaps

--- neovim/.config/nvim/lua/bradiate/core/keymaps.lua
+++ neovim/.config/nvim/lua/bradiate/core/keymaps.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Rex)
+
+-- Tabs
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "New Tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close Tab" })
+vim.keymap.set("n", "<C-l>", "<cmd>tabn<CR>", { desc = "Next Tab" })
+vim.keymap.set("n", "<C-h>", "<cmd>tabp<CR>", { desc = "Previous Tab" })
+vim.keymap.set("n", "<leader>te", "<cmd>tabnew .<CR>", { desc = "New tab in directory/explorer" })
