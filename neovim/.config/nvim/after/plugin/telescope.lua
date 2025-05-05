11a488f31f95bbb36f22ed72aa6eeb211f80783e
Brady Herbert <kbradyherbert@gmail.com>
2025-05-04 20:43:47 -0400

    fix(neovim): misc cleanup

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -4,9 +4,11 @@ local builtin = require('telescope.builtin')
 vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git Status" })
 
 -- Files
-vim.keymap.set('n', '<leader>pf', function()
+local function find_all_files()
   builtin.find_files({ hidden = true })
-end, { desc = 'Telescope find all files (including hidden)' })
+end
+
+vim.keymap.set('n', '<leader>pf', find_all_files, { desc = 'Telescope find all files (including hidden)' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope list buffers' })
 vim.keymap.set('n', '<leader>pr', builtin.oldfiles, { desc = 'Telescope find recent files' })
