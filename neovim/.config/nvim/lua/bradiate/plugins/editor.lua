170b7fef954d30c875520eab8a6551228b6b7505
Brady Herbert <kbradyherbert@gmail.com>
2025-05-04 20:48:25 -0400

    feat(neovim): handle whitespace, indent guide, ruler

--- neovim/.config/nvim/lua/bradiate/plugins/editor.lua
+++ neovim/.config/nvim/lua/bradiate/plugins/editor.lua
@@ -1,14 +1,57 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  config = function()
-    local autopairs = require("nvim-autopairs")
-
-    autopairs.setup({
-      check_ts = true,
-      ts_config = {
-        javascript = { "string", "template_string" },
+  {
+    'xiyaowong/virtcolumn.nvim',
+    event = { 'BufReadPre', 'BufNewFile' }
+  },
+	{
+    'mcauley-penney/visual-whitespace.nvim',
+    config = true,
+    event = "ModeChanged *:[vV\22]", -- optionally, lazy load on entering visual mode
+    opts = {
+      enabled = true,
+      highlight = { link = "Visual", default = true },
+      list_chars = {
+        space = '·',
+        tab = '→',
       },
-    })
-  end,
+      fileformat_chars = {
+        unix = '',
+      },
+      ignore = {
+        filetypes = {},
+        buftypes = {}
+      }
+    }
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {
+      indent = {
+        char = '▏',
+      },
+      scope = {
+        enabled = false,
+      },
+      exclude = {
+        filetypes = {
+          'netrw',
+        },
+      },
+    },
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      local autopairs = require("nvim-autopairs")
+
+      autopairs.setup({
+        check_ts = true,
+        ts_config = {
+          javascript = { "string", "template_string" },
+        },
+      })
+    end
+  },
 }
