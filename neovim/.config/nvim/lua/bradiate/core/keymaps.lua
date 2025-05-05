6393147a59d433aafff9c0a88577ca05a1448e0e
Brady Herbert <kbradyherbert@gmail.com>
2025-05-04 20:49:00 -0400

    fix(neovim): buffers, not tabs + keymaps

--- neovim/.config/nvim/lua/bradiate/core/keymaps.lua
+++ neovim/.config/nvim/lua/bradiate/core/keymaps.lua
@@ -1,13 +1,24 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Rex, { desc = "Open file explorer" })
 
+-- [Y]ank [P]ath to system clipboard
+local function copy_relative_path()
+  local relative_path = vim.fn.expand('%:p')
+  vim.fn.setreg('+', relative_path)
+  vim.notify('Copied: ' .. relative_path)
+end
+
+vim.keymap.set("n", "<leader>yp", copy_relative_path, { desc = "Copy path to clipboard" })
+
 -- Primeagen
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
 
+-- Buffers
+vim.keymap.set("n", "<C-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<C-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+
 -- Tabs
-vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "New Tab" })
-vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close Tab" })
-vim.keymap.set("n", "<C-l>", "<cmd>tabn<CR>", { desc = "Next Tab" })
-vim.keymap.set("n", "<C-h>", "<cmd>tabp<CR>", { desc = "Previous Tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "New tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
 vim.keymap.set("n", "<leader>te", "<cmd>tabnew .<CR>", { desc = "New tab in directory/explorer" })
--- neovim/.config/nvim/lua/bradiate/plugins/tabs.lua
+++ neovim/.config/nvim/lua/bradiate/plugins/tabs.lua
@@ -1,8 +1,17 @@
 return {
-  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons', opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "thin",
-    },
-  }},
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    local bufferline = require("bufferline")
+
+    bufferline.setup {
+      highlights = require("catppuccin.groups.integrations.bufferline").get(),
+      options = {
+        separator_style = 'thin',
+        always_show_bufferline = false,
+        mode = "buffers",
+      }
+    }
+  end
 }
