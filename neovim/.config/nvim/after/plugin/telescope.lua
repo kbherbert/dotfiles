1956a98442155bbe98a897620d8ee5ec61a68d8a
Brady Herbert <kbradyherbert@gmail.com>
2025-05-20 08:47:03 -0400

    fix(neovim): adjust telescope keymaps for git

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
 -- Git
-vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git Status" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Telescope git status" })
 
 -- Files
 local function find_all_files()
@@ -9,6 +9,7 @@ local function find_all_files()
 end
 
 vim.keymap.set('n', '<leader>pf', find_all_files, { desc = 'Telescope find all files (including hidden)' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Telescope find files in git project' })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files in git project' })
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope list buffers' })
 vim.keymap.set('n', '<leader>pr', builtin.oldfiles, { desc = 'Telescope find recent files' })
