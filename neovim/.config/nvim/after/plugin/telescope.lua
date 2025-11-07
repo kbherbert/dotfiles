11b16a4e87345f405adc5fcc2d46953e314f8be4
Brady Herbert <kbradyherbert@gmail.com>
2025-11-07 16:18:10 -0500

    feat(neovim): live grep functions

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -8,7 +8,27 @@ local function find_all_files()
   builtin.find_files({ hidden = true })
 end
 
-vim.keymap.set('n', '<leader>pf', find_all_files, { desc = 'Telescope find all files (including hidden)' })
+local function live_grep()
+  builtin.live_grep({
+    additional_args = function(opts)
+      return { "--hidden" }
+    end,
+  })
+end
+
+local function grep_word_under_cursor()
+  local word = vim.fn.expand("<cword>")
+  builtin.grep_string({
+    search = word,
+    additional_args = function(opts)
+      return { "--hidden" }
+    end,
+  })
+end
+
+vim.keymap.set('n', '<leader>ff', find_all_files, { desc = 'Telescope find all files (including hidden)' })
+vim.keymap.set('n', '<leader>fw', live_grep, { desc = 'Telescope live grep (including hidden)' })
+vim.keymap.set('v', 'F', grep_word_under_cursor, { desc = 'Telescope grep word under cursor in visual mode' })
 vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Telescope find files in git project' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files in git project' })
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope list buffers' })
