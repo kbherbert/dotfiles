3c0e5ca719aaf060c1668b6b675207d70310bbc2
Brady Herbert <kbradyherbert@gmail.com>
2026-05-07 15:18:50 -0400

    chore: lock file maintenance + treesitter/telescope upgrade

--- neovim/.config/nvim/lua/bradiate/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/bradiate/plugins/treesitter.lua
@@ -1,15 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = { "lua", "vim", "javascript", "html", "typescript" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+  lazy = false,
+  build = ":TSUpdate"
 }
