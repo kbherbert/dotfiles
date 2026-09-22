c46672e464153886b89f9f898ac6d0376de47d99
Brady Herbert <1534912+kbherbert@users.noreply.github.com>
2026-09-22 14:05:15 -0400

    fix(neovim): telescope gs doesn't show on small screens

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,10 +1,9 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 -- Git
 vim.keymap.set("n", "<leader>gs", function()
   builtin.git_status({
-    layout_strategy = "vertical", 
-    layout_config = { preview_height = 0.6 },
+    layout_strategy = "flex",
   })
 end, { desc = "Telescope git status" })
 
@@ -15,7 +14,7 @@ end
 
 local function live_grep()
   builtin.live_grep({
-    additional_args = function(opts)
+    additional_args = function()
       return { "--hidden" }
     end,
   })
@@ -25,16 +24,16 @@ local function grep_word_under_cursor()
   local word = vim.fn.expand("<cword>")
   builtin.grep_string({
     search = word,
-    additional_args = function(opts)
+    additional_args = function()
       return { "--hidden" }
     end,
   })
 end
 
-vim.keymap.set('n', '<leader>ff', find_all_files, { desc = 'Telescope find all files (including hidden)' })
-vim.keymap.set('n', '<leader>fw', live_grep, { desc = 'Telescope live grep (including hidden)' })
-vim.keymap.set('v', 'F', grep_word_under_cursor, { desc = 'Telescope grep word under cursor in visual mode' })
-vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = 'Telescope find files in git project' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files in git project' })
-vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope list buffers' })
-vim.keymap.set('n', '<leader>pr', builtin.oldfiles, { desc = 'Telescope find recent files' })
+vim.keymap.set("n", "<leader>ff", find_all_files, { desc = "Telescope find all files (including hidden)" })
+vim.keymap.set("n", "<leader>fw", live_grep, { desc = "Telescope live grep (including hidden)" })
+vim.keymap.set("v", "F", grep_word_under_cursor, { desc = "Telescope grep word under cursor in visual mode" })
+vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Telescope find files in git project" })
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find files in git project" })
+vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Telescope list buffers" })
+vim.keymap.set("n", "<leader>pr", builtin.oldfiles, { desc = "Telescope find recent files" })
