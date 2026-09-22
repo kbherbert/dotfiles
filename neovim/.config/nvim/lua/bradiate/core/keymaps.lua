07478d242e0571240ea7443653e67452bb312e2a
Brady Herbert <1534912+kbherbert@users.noreply.github.com>
2026-09-22 14:39:34 -0400

    feat(neovim): smart yank path

--- neovim/.config/nvim/lua/bradiate/core/keymaps.lua
+++ neovim/.config/nvim/lua/bradiate/core/keymaps.lua
@@ -1,13 +1,32 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Rex, { desc = "Open file explorer" })
 
 -- [Y]ank [P]ath to system clipboard
-local function copy_relative_path()
-  local relative_path = vim.fn.expand('%:p')
-  vim.fn.setreg('+', relative_path)
-  vim.notify('Copied: ' .. relative_path)
+local function copy_project_relative_path()
+  local file_path = vim.api.nvim_buf_get_name(0)
+  if file_path == "" then
+    vim.notify("No file path available for current buffer", vim.log.levels.WARN)
+    return
+  end
+
+  local file_dir = vim.fn.fnamemodify(file_path, ":h")
+  local git_root_result = vim.fn.systemlist({ "git", "-C", file_dir, "rev-parse", "--show-toplevel" })
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Current file is not inside a Git repository", vim.log.levels.WARN)
+    return
+  end
+
+  local git_root = git_root_result[1]
+  local relative_path = vim.fs.relpath(git_root, file_path)
+  if not relative_path then
+    vim.notify("Unable to determine path relative to Git root", vim.log.levels.WARN)
+    return
+  end
+
+  vim.fn.setreg("+", relative_path)
+  vim.notify("Copied: " .. relative_path)
 end
 
-vim.keymap.set("n", "<leader>yp", copy_relative_path, { desc = "Copy path to clipboard" })
+vim.keymap.set("n", "<leader>yp", copy_project_relative_path, { desc = "Copy path to clipboard" })
 
 -- Primeagen
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
