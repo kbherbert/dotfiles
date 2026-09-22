5a73c6c270fb29b38d2b43c373ff5d3cd41e107e
Brady Herbert <1534912+kbherbert@users.noreply.github.com>
2026-09-22 15:53:43 -0400

    fix(neovim): show only recent files within git repo

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -30,10 +30,30 @@ local function grep_word_under_cursor()
   })
 end
 
+local function project_oldfiles()
+  local cwd = vim.fn.getcwd()
+  local git_root = vim.fn.systemlist({
+    "git",
+    "-C",
+    cwd,
+    "rev-parse",
+    "--show-toplevel",
+  })[1]
+
+  if git_root and git_root ~= "" then
+    cwd = git_root
+  end
+
+  builtin.oldfiles({
+    cwd = cwd,
+    only_cwd = true,
+  })
+end
+
 vim.keymap.set("n", "<leader>ff", find_all_files, { desc = "Telescope find all files (including hidden)" })
 vim.keymap.set("n", "<leader>fw", live_grep, { desc = "Telescope live grep (including hidden)" })
 vim.keymap.set("v", "F", grep_word_under_cursor, { desc = "Telescope grep word under cursor in visual mode" })
 vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Telescope find files in git project" })
 vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find files in git project" })
 vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Telescope list buffers" })
-vim.keymap.set("n", "<leader>pr", builtin.oldfiles, { desc = "Telescope find recent files" })
+vim.keymap.set("n", "<leader>pr", project_oldfiles, { desc = "Telescope find recent files" })
